7be9d58984595f9d2cc677dc722edaee0cadbf9e
Romans Malinovskis <r@agiletech.ie>
2013-02-14 02:05:54 +0000

    fix

--- config-default.php
+++ config-default.php
@@ -4,12 +4,12 @@ $config['atk']['base_path']='/atk4/';
 $config['url_prefix']='?page=';
 $config['url_postfix']='';
 
-    $config['opauth']=array(
-        // See: https://github.com/uzyn/opauth/wiki/Opauth-configuration
-        'Strategy'=>array(
-            'github'=>array(
-                'client_id'=>'09348363e5e10cf3a7ba',
-                'client_secret'=>'432433a4f4418245dd8efcfd898f08e291b323d1'
-            )
+$config['opauth']=array(
+    // See: https://github.com/uzyn/opauth/wiki/Opauth-configuration
+    'Strategy'=>array(
+        'GitHub'=>array(
+            'client_id'=>'09348363e5e10cf3a7ba',
+            'client_secret'=>'432433a4f4418245dd8efcfd898f08e291b323d1'
         )
-    );
+    )
+);
